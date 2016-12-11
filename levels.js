0b0524d9e37b28afe14e1e4c01c78f7ed37d9ec4
pytlyk <pytlyk96@gmail.com>
2016-12-11 18:54:25 +0200

    Color for board of  game

---
+++ levels.js
@@ -0,0 +1,47 @@
+Breakout.Colors = {
+
+  arkanoid: {
+    w: "#FCFCFC", // white
+    o: "#FC7460", // orange
+    l: "#3CBCFC", // light blue
+    g: "#80D010", // green
+    r: "#D82800", // red
+    b: "#0070EC", // blue
+    p: "#FC74B4", // pink
+    y: "#FC9838", // yellow
+    s: "#BCBCBC", // silver
+    d: "#F0BC3C"  // gold
+  },
+
+  pastel: {
+    y: "#FFF7A5", // yellow
+    p: "#FFA5E0", // pink
+    b: "#A5B3FF", // blue
+    g: "#BFFFA5", // green
+    o: "#FFCBA5"  // orange
+  },
+
+  vintage: {
+    a: "#EFD279", // yellow
+    b: "#95CBE9", // light blue
+    c: "#024769", // dark blue
+    d: "#AFD775", // light green
+    e: "#2C5700", // grass
+    f: "#DE9D7F", // red
+    g: "#7F9DDE", // purple
+    h: "#00572C", // dark green
+    i: "#75D7AF", // mint
+    j: "#694702", // brown
+    k: "#E9CB95", // peach
+    l: "#79D2EF"  // blue
+  },
+
+  liquidplanner: {
+    a: '#62C4E7', // light blue
+    b: '#00A5DE', // dark  blue
+    x: '#969699', // light gray
+    y: '#7B797E'  // dark  gray
+  },
+
+
+};
